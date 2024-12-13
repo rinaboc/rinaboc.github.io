270a55cbdd7441b404173fcfb10f697375780827
bocska.karina <bocska.karina@hallgato.ppke.hu>
2024-12-13 16:58:16 +0100

    add under development page

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import otter from "./assets/walking-otter-noground.gif";
+import runningOtter from "./assets/running-otter.gif";
+import idleOtter from "./assets/idle-otter.gif";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const getSprite = () => {
+    if (count >= 69) return runningOtter;
+    if (count >= 10) return otter;
+    return idleOtter;
+  };
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={getSprite()} className="sprite" width={102} />
       </div>
-      <h1>Vite + React</h1>
+      <h1>Under Development ...</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
